6c2b28bfe152771240c615bcc4d46a75b2fb1163
Marian54 <85902371+Marian54@users.noreply.github.com>
2021-11-02 22:56:19 +0200

    Update terraform.tf

--- infra/terraform.tf
+++ infra/terraform.tf
@@ -2,6 +2,13 @@ terraform {
 
   required_version = ">=0.12"
 
+  backend "azurerm" {
+    resource_group_name  = "rg-azure-tf"
+    storage_account_name = "tfstate4711"
+    container_name       = "tfstate"
+    key                  = "azure-tf.terraform.tfstate"
+  }
+  
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
