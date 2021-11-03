eb7e0f393d25d3c0dabcaae41b2f4455fa4d1d3f
Marian54 <85902371+Marian54@users.noreply.github.com>
2021-11-03 09:14:45 +0200

    Delete infra directory

--- infra/azurerm_subnet.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_subnet" "subnet" {
-  name                 = "${var.prefix}-subnet"
-  resource_group_name  =  azurerm_resource_group.rg-azure-tf.name
-  virtual_network_name = azurerm_virtual_network.vmtf_vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
