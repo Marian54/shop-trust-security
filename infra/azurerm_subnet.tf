733f2c3d1aa355a03d7a4ada0c0c135793c8307e
Marian54 <85902371+Marian54@users.noreply.github.com>
2021-11-03 22:55:08 +0200

    Update azurerm_subnet.tf

--- infra/azurerm_subnet.tf
+++ infra/azurerm_subnet.tf
@@ -1,6 +1,6 @@
-resource "azurerm_subnet" "subnet" {
+resource "azurerm_subnet" "subnet-Team2" {
   name                 = "${var.prefix}-subnet"
-  resource_group_name  =  azurerm_resource_group.rg-azure-tf.name
-  virtual_network_name = azurerm_virtual_network.vmtf_vnet.name
+  resource_group_name  =  azurerm_resource_group.Resource-Group-Azure-tf-Team2.name
+  virtual_network_name = azurerm_virtual_network.vmtf_vnet_Team2.name
   address_prefixes     = ["10.0.2.0/24"]
 }
