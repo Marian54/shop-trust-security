3afbedbfa938733513ce8fb9b3feba7254341c34
Marian54 <85902371+Marian54@users.noreply.github.com>
2021-11-03 22:54:18 +0200

    Update azurerm_public_ip.tf

--- infra/azurerm_public_ip.tf
+++ infra/azurerm_public_ip.tf
@@ -1,7 +1,7 @@
-resource "azurerm_public_ip" "vmtf_pip" {
-  name                = "${var.prefix}-ip"
+resource "azurerm_public_ip" "vmtf_pip_Team2" {
+  name                = "${var.prefix}-public-ip"
   location            = var.resource_group_location
-  resource_group_name =  azurerm_resource_group.rg-azure-tf.name
+  resource_group_name =  azurerm_resource_group.Resource-Group-Azure-tf-Team2.name
   allocation_method   = "Dynamic"
   domain_name_label   = var.hostname
 }
