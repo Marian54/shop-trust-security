f31b68dcc172f950699dbb53fd6c8eb84a6d4586
Marian54 <85902371+Marian54@users.noreply.github.com>
2021-11-03 22:52:50 +0200

    Update azurerm_linux_virtual_machine.tf

--- infra/azurerm_linux_virtual_machine.tf
+++ infra/azurerm_linux_virtual_machine.tf
@@ -1,21 +1,27 @@
 resource "tls_private_key" "example_ssh" {
-    algorithm = "RSA"
-    rsa_bits = 4096
+  algorithm = "RSA"
+  rsa_bits = 4096
 }
 
-resource "azurerm_linux_virtual_machine" "vmtf_site" {
+resource "azurerm_linux_virtual_machine" "VirtualMachine_Terraform_Team_2" {
   name                = "${var.hostname}-site"
-  resource_group_name =  azurerm_resource_group.rg-azure-tf.name
+  resource_group_name =  azurerm_resource_group.Resource-Group-Azure-tf-Team2.name
   location            = var.resource_group_location
   size             = var.vm_size
-  network_interface_ids         = [azurerm_network_interface.vmtf_nic.id]
-
+  network_interface_ids         = [azurerm_network_interface.vmtf_nic_Team2.id]
 
 
   admin_ssh_key {
     username   = var.admin_username
-    public_key = tls_private_key.example_ssh.public_key_openssh 
+    public_key = tls_private_key.example_ssh.public_key_openssh
   }
+
+  /*
+    admin_ssh_key {
+      username   = var.admin_username
+      public_key = file("${path.module}/id_rsa3.pub")
+    }
+  */
 
   source_image_reference  {
     publisher = "Canonical"
@@ -43,7 +49,7 @@ resource "azurerm_linux_virtual_machine" "vmtf_site" {
       private_key = file("${path.module}/id_rsa3")
       agent = false
     }
-  }**/
+  }*/
 
   admin_username = "azureuser"
 }
