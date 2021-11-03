4d0ad4a701baf40f852d9b815a6b5e267fb49cbb
Marian54 <85902371+Marian54@users.noreply.github.com>
2021-11-03 22:53:24 +0200

    Update azurerm_network_interface.tf

--- infra/azurerm_network_interface.tf
+++ infra/azurerm_network_interface.tf
@@ -1,11 +1,11 @@
-resource "azurerm_network_interface" "vmtf_nic" {
-  name                = "${var.prefix}-nic"
+resource "azurerm_network_interface" "vmtf_nic_Team2" {
+  name                = "${var.prefix}-Network_interface"
   location            = var.resource_group_location
-  resource_group_name =  azurerm_resource_group.rg-azure-tf.name
+  resource_group_name =  azurerm_resource_group.Resource-Group-Azure-tf-Team2.name
   ip_configuration {
     name                          = "temaip"
-    subnet_id                     = azurerm_subnet.subnet.id
+    subnet_id                     = azurerm_subnet.subnet-Team2.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.vmtf_pip.id
+    public_ip_address_id          = azurerm_public_ip.vmtf_pip_Team2.id
   }
 }
