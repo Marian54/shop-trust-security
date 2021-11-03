8cf35c26ee29d543aefd00ea5a091adea83569a9
Marian54 <85902371+Marian54@users.noreply.github.com>
2021-11-03 22:54:45 +0200

    Update azurerm_resource_group.tf

--- infra/azurerm_resource_group.tf
+++ infra/azurerm_resource_group.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "rg-azure-tf" {
+resource "azurerm_resource_group" "Resource-Group-Azure-tf-Team2" {
   name = var.resource_group_name
   location = var.resource_group_location
 }
