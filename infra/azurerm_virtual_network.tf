68851cf8c92db63b4620ebb871cc6f7ced1b92ec
Marian54 <85902371+Marian54@users.noreply.github.com>
2021-11-03 22:55:31 +0200

    Update azurerm_virtual_network.tf

--- infra/azurerm_virtual_network.tf
+++ infra/azurerm_virtual_network.tf
@@ -1,6 +1,6 @@
-resource "azurerm_virtual_network" "vmtf_vnet" {
+resource "azurerm_virtual_network" "vmtf_vnet_Team2" {
   name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/16"]
   location            = var.resource_group_location
-  resource_group_name =  azurerm_resource_group.rg-azure-tf.name
+  resource_group_name =  azurerm_resource_group.Resource-Group-Azure-tf-Team2.name
 }
